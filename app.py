a6b14924747f49fe0e3686da60808136f631d445
Michael Scheid <michaelscheid2015@u.northwestern.edu>
2020-03-17 20:41:25 -0400

    debug deploy

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ def graph():
                alpha=0.5, 
                legend_label='%s: Closing price'% tick)
         
-        del data1, feature1, app.vars['feature1']
+        del feature1, app.vars['feature1']
                
     # Check if feature2 was selected, if so query Quandl API and plot   
     if 'feature2' in app.vars.keys():
@@ -97,7 +97,7 @@ def graph():
                alpha=0.5, 
                legend_label='%s: Adj Closing price'% tick)
                
-        del data2, feature2, app.vars['feature2']
+        del feature2, app.vars['feature2']
     
     # Create the HTML script to pass and embed in the graph.html page
     script, div = components(p)
